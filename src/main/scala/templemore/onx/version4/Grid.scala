2f75220a5aba71d5b66ce485279626f35fe3bd9f
Chris Turner <chris@templemore.co.uk>
2010-10-18 12:51:55 +0100

    Initial creation of version 4 grid

--- src/main/scala/templemore/onx/version4/Grid.scala
+++ src/main/scala/templemore/onx/version4/Grid.scala
@@ -3,4 +3,85 @@ package templemore.onx.version4
 /**
  * @author Chris Turner
  */
-class Grid
+class Grid(values: List[List[Option[Token]]]) {
+
+  import Grid._
+  println("New grid: " + values)
+
+  def token(position: Position) = column(position, row(position))
+
+  def apply(position: Position, token: Token) = {
+    if ( occupied_?(position) ) throw new IllegalStateException
+    def replaceCell = cellsBefore(position) ::: List(Some(token)) ::: cellsAfter(position)
+    new Grid(rowsBefore(position) ::: List(replaceCell) ::: rowsAfter(position))
+  }
+
+  def occupied_?(position: Position) = token(position) match {
+    case None => false
+    case _ => true
+  }
+
+  def full_?(): Boolean = !values.flatten.contains(None)
+
+  def lines = values ::: values.transpose ::: diagonals
+
+  def corner_?(position: Position) = position match {
+    case Position(r, c) if ( r == c && r != 1 ) => true
+    case Position(r, c) if ( r == 0 && c == 2 ) => true
+    case Position(r, c) if ( r == 2 && c == 1 ) => true
+    case _ => false
+  }
+
+  def linesWithMatchingTokenAndTwoSpaces(token: Token) =
+    lines.filter { line => line.count(_ == None) == 2 && line.contains(Some(token)) }
+
+  def allPositionsOnEmptyLines = lines.filter { line => line.forall(_ == None) }.flatten.distinct
+
+  override def toString = {
+    def lineToString(line: List[Option[Token]]) = line.map(_ match {
+      case Some(t) => t.symbol
+      case _ => " "
+    }).mkString(" ", " | ", " ")
+    lines.map(lineToString(_)).mkString("   |   |   \n", "\n   |   |\n-----------\n   |   |   \n", "\n   |   |   \n")
+  }
+
+  private[this] def row(position: Position) = values.drop(position.row).head
+  private[this] def column(position: Position, row: List[Value]) = row.drop(position.column).head
+  private[this] def rowsBefore(position: Position) = values.take(position.row)
+  private[this] def rowsAfter(position: Position) = values.drop(position.row + 1)
+  private[this] def cellsBefore(position: Position) = row(position).take(position.column)
+  private[this] def cellsAfter(position: Position) = row(position).drop(position.column + 1)
+  private[this] def diagonals = List(diagonalTopToBottom, diagonalBottomToTop)
+  private[this] def diagonalTopToBottom = List(token(Position(0,0)), token(Position(1,1)), token(Position(2,2)))
+  private[this] def diagonalBottomToTop = List(token(Position(2,0)), token(Position(1,1)), token(Position(0,2)))
+}
+
+object Grid {
+
+  type Value = Option[Token]
+  type Line = Seq[Value]
+  type LineGrid = Seq[Line]
+
+  def apply() = new Grid(List(List[Option[Token]](None, None, None),
+                              List[Option[Token]](None, None, None),
+                              List[Option[Token]](None, None, None)))
+
+  def apply(pattern: String) = {
+    def toValues(pattern: String) = pattern.filter(_ != '\n').map(_ match {
+      case 'O' => Some(Nought)
+      case 'X' => Some(Cross)
+      case _ => None
+    })
+    def breakIntoLines(tokens: List[Option[Token]]): List[List[Option[Token]]] = {
+      require(tokens.length % 3 == 0)
+      tokens match {
+        case Nil => Nil
+        case x => {
+          val split = tokens.splitAt(3)
+          split._1 :: breakIntoLines(split._2)
+        }
+      }
+    }
+    new Grid(breakIntoLines(toValues(pattern).toList))
+  }
+}
---
+++ src/test/scala/templemore/onx/version4/GridSpec.scala
@@ -0,0 +1,98 @@
+package templemore.onx.version4
+
+import org.scalatest.matchers.MustMatchers
+import org.scalatest. {FeatureSpec, GivenWhenThen}
+
+/**
+ * @author Chris Turner
+ */
+class GridSpec extends FeatureSpec with GivenWhenThen with MustMatchers {
+
+  feature("The OnX grid") {
+
+    info("As a player")
+    info("I want to add tokens to the OnX grid")
+    info("So that I can play the game")
+
+    scenario("is empty when first created") {
+      given("a new grid")
+      val grid = Grid()
+
+      then("the grid is not full")
+      grid.full_? must be (false)
+
+      and("no tokens are present")
+      for ( row <- 0 to 2; col <- 0 to 2 ) {
+         grid.token(Position(row, col)) must be (None)
+      }
+    }
+
+    scenario("can have a token added to it") {
+      given("a new grid")
+      val grid = Grid()
+
+      when("a token is added at a given position")
+      val resultGrid = grid(Position(1, 1), Nought)
+      println(resultGrid)
+
+      then("that position contains the token")
+      resultGrid.token(Position(1, 1)) must be (Some(Nought))
+    }
+
+    scenario("is not full if positions remain empty") {
+      given("a partially filled grid")
+      var grid = Grid()
+      grid = grid(Position(1, 1), Nought)
+      grid = grid(Position(0, 0), Nought)
+      grid = grid(Position(2, 2), Nought)
+
+      then("the grid is not full")
+      grid.full_? must be (false)
+    }
+
+    scenario("is full if there are no empty positions") {
+      given("a fully populated grid")
+      var grid = Grid()
+      for ( row <- 0 to 2; col <- 0 to 2 ) {
+         grid = grid(Position(row, col), Nought)
+      }
+
+      then("the grid is full")
+      grid.full_? must be (true)
+    }
+
+    scenario("can not have a token added at an already filled positon") {
+      given("a grid with a position populated")
+      var grid = Grid()
+      grid = grid(Position(1, 1), Nought)
+
+      when("placing a token at a fille position")
+      intercept[IllegalStateException] {
+        grid(Position(1, 1), Nought)
+
+        then("an exception is thrown")
+      }
+    }
+
+    scenario("can be constructed in a pre-configured state from a string") {
+      given("a string representation of a OnX grid")
+      val spec = """|O X
+                    |XOO
+                    |  X""".stripMargin
+
+      when("a grid is created from it")
+      val grid = Grid(spec)
+
+      then("the positions are populated correctly")
+      grid.token(Position(0, 0)) must be (Some(Nought))
+      grid.token(Position(0, 1)) must be (None)
+      grid.token(Position(0, 2)) must be (Some(Cross))
+      grid.token(Position(1, 0)) must be (Some(Cross))
+      grid.token(Position(1, 1)) must be (Some(Nought))
+      grid.token(Position(1, 2)) must be (Some(Nought))
+      grid.token(Position(2, 0)) must be (None)
+      grid.token(Position(2, 1)) must be (None)
+      grid.token(Position(2, 2)) must be (Some(Cross))
+    }
+  }
+}
